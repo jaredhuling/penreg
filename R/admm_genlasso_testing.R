d448364d5b00bbba1292e48bc310b01dcc24dc78
jaredhuling <huling@wisc.edu>
2016-02-15 20:54:34 -0600

    added generalized lasso testing code. completed both linear regression and logistic regression. both work

--- R/admm_genlasso_testing.R
+++ R/admm_genlasso_testing.R
@@ -1,34 +1,42 @@
 
 
-admm.lasso.R <- function(x, y, lambda, rho, abs.tol = 1e-5, rel.tol = 1e-5, maxit = 500L, gamma = 4) {
+admm.genlasso.R <- function(x, y, D, lambda, rho, abs.tol = 1e-5, rel.tol = 1e-5, maxit = 500L, gamma = 4) {
     library(Matrix)
-    xtx <- crossprod(x)
-    xty <- crossprod(x, y)
-    loss.history <- rep(NA, maxit)
     n <- length(y)
     p <- ncol(x)
+    npen <- nrow(D)
+    if (ncol(D) != p) {
+        stop("D does not have correct number of columns")
+    }
+    xtx <- crossprod(x)
+    DtD <- crossprod(D)
+    xty <- crossprod(x, y)
+    loss.history <- rep(NA, maxit)
+    
     #lambda <- lambda * n
     iters <- maxit
     
-    alpha <- z <- u <- numeric(p)
-    A <- as(xtx + rho * diag(p), "Matrix")
+    alpha <- numeric(p)
+    z <- u <- numeric(npen)
+    A <- as(xtx + rho * DtD, "Matrix")
     
     for (i in 1:maxit) {
-        q <- xty + rho * (z - u)
+        q <- xty + rho * crossprod(D, (z - u))
         alpha <- as.vector(solve(A, q))
         z.prev <- z
         
         #z <- sign(alpha + u) * pmax(abs(alpha + u) - lambda / rho, 0) / (1 - 1/gamma)
         #z <- ifelse( abs(alpha + u) <= gamma * (lambda/rho), z, alpha + u)
         
-        z <- soft.thresh(alpha + u, lambda / rho)
+        Dalpha <- D %*% alpha
+        z <- soft.thresh(Dalpha + u, lambda / rho)
         
-        u <- u + (alpha - z)
+        u <- u + (Dalpha - z)
         loss.history[i] <- l1.loss.leastsquares(x, y, alpha, z, lambda)
         
-        r_norm = sqrt(sum( (alpha - z)^2 ))
+        r_norm = sqrt(sum( (Dalpha - z)^2 ))
         s_norm = sqrt(sum( (-rho * (z - z.prev))^2 ))
-        eps_pri = sqrt(p) * abs.tol + rel.tol * max(sqrt(sum(alpha ^ 2)), sqrt(sum((-z)^2 ) ))
+        eps_pri = sqrt(p) * abs.tol + rel.tol * max(sqrt(sum(Dalpha ^ 2)), sqrt(sum((-z)^2 ) ))
         eps_dual = sqrt(p) * abs.tol + rel.tol * sqrt(sum( (rho * u)^2 ))
         
         
@@ -38,7 +46,7 @@ admm.lasso.R <- function(x, y, lambda, rho, abs.tol = 1e-5, rel.tol = 1e-5, maxi
         }
     }
     
-    list(beta=z, iters = iters, loss.hist = loss.history[!is.na(loss.history)])
+    list(beta=alpha, iters = iters, loss.hist = loss.history[!is.na(loss.history)], beta.aug = z)
 }
 
 
@@ -66,48 +74,57 @@ l1.loss.logistic2 <- function(x, y, beta, z, lambda)
 }
 
 
-
-admm.lasso.logistic.R <- function(x, y, lambda, rho, abs.tol = 1e-5, rel.tol = 1e-5, maxit = 500L, gamma = 4) {
+##
+admm.genlasso.logistic.R <- function(x, y, D, lambda, rho, 
+                                     abs.tol = 1e-5, rel.tol = 1e-5, 
+                                     maxit = 500L, gamma = 4) {
     library(Matrix)
     #xtx <- crossprod(x)
     xty <- crossprod(x, y)
     loss.history <- rep(NA, maxit)
     n <- length(y)
     p <- ncol(x)
+    npen <- nrow(D)
+    if (ncol(D) != p) {
+        stop("D does not have correct number of columns")
+    }
     #lambda <- lambda * n
+    DtD <- crossprod(D)
     iters <- maxit
     
-    alpha <- z <- u <- numeric(p)
+    alpha <- numeric(p)
+    z <- u <- numeric(npen)
     
     for (i in 1:maxit) {
         
         # update alpha
-        alpha <- logistic.x.update(x, xty, u, z, rho, alpha.0 = alpha)
+        alpha <- logistic.gen.x.update(x, xty, u, z, rho, D, DtD, alpha.0 = alpha)
         z.prev <- z
         
+        
         # update z
-        z <- soft.thresh(alpha + u, lambda / rho)
+        Dalpha <- D %*% alpha
+        z <- soft.thresh(Dalpha + u, lambda / rho)
         
         # update lagrangian parameter
-        u <- u + (alpha - z)
+        u <- u + (Dalpha - z)
         
         loss.history[i] <- l1.loss.logistic(x, y, alpha, z, lambda)
         
-        r_norm   = sqrt(sum( (alpha - z)^2 ))
+        r_norm   = sqrt(sum( (Dalpha - z)^2 ))
         s_norm   = sqrt(sum( (-rho * (z - z.prev))^2 ))
-        eps_pri  = sqrt(p) * abs.tol + rel.tol * max(sqrt(sum(alpha ^ 2)), sqrt(sum((-z)^2 ) ))
+        eps_pri  = sqrt(p) * abs.tol + rel.tol * max(sqrt(sum(Dalpha ^ 2)), sqrt(sum((-z)^2 ) ))
         eps_dual = sqrt(p) * abs.tol + rel.tol * sqrt(sum( (rho * u)^2 ))
         
-        if(r_norm  > 10 * s_norm )
+        if(r_norm / eps_pri > 10 * s_norm / eps_dual)
         {
-            rho <- rho * (2)
+            rho <- rho * 2
             u <- u * 0.5
-        } else if(s_norm  > 10 * r_norm )
+        } else if(s_norm / eps_dual > 10 * r_norm / eps_pri)
         {
-            rho <- rho * 0.5
+            rho <- rho / 2
             u <- u * 2
         }
-        
         
         
         if (r_norm < eps_pri & s_norm < eps_dual) {
@@ -116,12 +133,12 @@ admm.lasso.logistic.R <- function(x, y, lambda, rho, abs.tol = 1e-5, rel.tol = 1
         }
     }
     
-    list(beta=z, iters = iters, loss.hist = loss.history[!is.na(loss.history)])
+    list(beta=alpha, iters = iters, loss.hist = loss.history[!is.na(loss.history)], beta.aug = z)
 }
 
 
 # admm x update step for logistic regression
-logistic.x.update <- function(x, xty, u, z, rho, alpha.0 = NULL)
+logistic.gen.x.update <- function(x, xty, u, z, rho, D, DtD, alpha.0 = NULL)
 {
     nvars <- ncol(x)
     if (is.null(alpha.0))
@@ -136,9 +153,9 @@ logistic.x.update <- function(x, xty, u, z, rho, alpha.0 = NULL)
     for (i in 1:niter)
     {
         prob <- drop(1 / (1 + exp( -x %*% alpha.cur)))
-        grad <- drop(crossprod(x, prob) - xty + rho * (alpha.cur - z + u))
+        grad <- drop(   crossprod(x, prob) - xty + rho * crossprod(D, D %*% alpha.cur  -z + u)   )
         W    <- prob * (1 - prob)
-        HH   <- crossprod(x * sqrt(W), x) + rho * diag(nvars)
+        HH   <- crossprod(x * sqrt(W), x) + rho * DtD
         dx   <- -1 * solve(HH, grad)
         dfx  <- crossprod(grad, dx)
         if (abs(dfx) < tol)

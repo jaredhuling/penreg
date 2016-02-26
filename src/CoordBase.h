040a610018ff201f7b94f74be81916b1cd8a2e9e
jaredhuling <huling@wisc.edu>
2016-02-26 00:10:18 -0600

    naive and working coordinate descent lasso (least squares) implementation up

---
+++ src/CoordBase.h
@@ -0,0 +1,89 @@
+#ifndef COORDBASE_H
+#define COORDBASE_H
+
+#include <RcppEigen.h>
+#include "Linalg/BlasWrapper.h"
+#include "utils.h"
+
+
+template<typename VecTypeX>
+class CoordBase
+{
+protected:
+    
+    const int nvars;   // dimension of beta
+    const int nobs;    // number of rows
+    
+    VecTypeX beta;        // parameters to be optimized
+    VecTypeX beta_prev;       // auxiliary parameters
+    
+    
+    double tol;           // tolerance for primal residual
+    
+    virtual void next_beta(VecTypeX &res) = 0;
+    
+    void print_row(int iter)
+    {
+        const char sep = ' ';
+        
+        Rcpp::Rcout << std::left << std::setw(7)  << std::setfill(sep) << iter;
+        Rcpp::Rcout << std::endl;
+    }
+    void print_footer()
+    {
+        const int width = 80;
+        Rcpp::Rcout << std::string(width, '=') << std::endl << std::endl;
+    }
+    
+public:
+    CoordBase(int n_, int p_,
+              double tol_ = 1e-6) :
+    nvars(p_), nobs(n_),
+    beta(p_), beta_prev(p_), // allocate space but do not set values
+    tol(tol_)
+    {}
+    
+    virtual ~CoordBase() {}
+    
+    void update_beta()
+    {
+        //VecTypeX newbeta(nvars);
+        next_beta(beta);
+        //beta.swap(newbeta);
+    }
+    bool converged()
+    {
+        return (stopRule(beta, beta_prev, tol));
+    }
+    
+    int solve(int maxit)
+    {
+        int i;
+        
+        for(i = 0; i < maxit; ++i)
+        {
+            beta_prev = beta;
+            // old_y = dual_y;
+            //std::copy(dual_y.data(), dual_y.data() + dim_dual, old_y.data());
+            
+            update_beta();
+            
+            // print_row(i);
+            
+            if(converged())
+                break;
+            
+        }
+        
+        // print_footer();
+        
+        return i + 1;
+    }
+    
+    virtual VecTypeX get_beta() { return beta; }
+};
+
+
+
+#endif // COORDBASE_H
+
---
+++ src/CoordLasso.h
@@ -0,0 +1,158 @@
+#ifndef COORDLASSOTALL_H
+#define COORDLASSOTALL_H
+
+#include "CoordBase.h"
+#include "Linalg/BlasWrapper.h"
+#include "ADMMMatOp.h"
+#include "utils.h"
+
+// minimize  1/2 * ||y - X * beta||^2 + lambda * ||beta||_1
+//
+// In ADMM form,
+//   minimize f(x) + g(z)
+//   s.t. x - z = 0
+//
+// x => beta
+// z => -X * beta
+// A => X
+// b => y
+// f(x) => 1/2 * ||Ax - b||^2
+// g(z) => lambda * ||z||_1
+class CoordLasso: public CoordBase<Eigen::VectorXd>
+{
+protected:
+    typedef float Scalar;
+    typedef double Double;
+    typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> Matrix;
+    typedef Eigen::Matrix<double, Eigen::Dynamic, 1> Vector;
+    typedef Eigen::Map<const Matrix> MapMat;
+    typedef Eigen::Map<const Vector> MapVec;
+    typedef const Eigen::Ref<const Matrix> ConstGenericMatrix;
+    typedef const Eigen::Ref<const Vector> ConstGenericVector;
+    typedef Eigen::SparseMatrix<double> SpMat;
+    typedef Eigen::SparseVector<double> SparseVector;
+    
+    MapMat datX;                  // data matrix
+    MapVec datY;                  // response vector
+    Vector XY;                    // X'Y
+    MatrixXd Xsq;                 // colSums(X^2)
+    
+    Scalar lambda;                // L1 penalty
+    Scalar lambda0;               // minimum lambda to make coefficients all zero
+    
+    /*
+    static void soft_threshold(SparseVector &res, const Vector &vec, const double &penalty)
+    {
+        int v_size = vec.size();
+        res.setZero();
+        res.reserve(v_size);
+        
+        const double *ptr = vec.data();
+        for(int i = 0; i < v_size; i++)
+        {
+            if(ptr[i] > penalty)
+                res.insertBack(i) = ptr[i] - penalty;
+            else if(ptr[i] < -penalty)
+                res.insertBack(i) = ptr[i] + penalty;
+        }
+    }
+    */
+    
+    static double soft_threshold(double &value, const double &penalty)
+    {
+        if(value > penalty)
+            return(value - penalty);
+        else if(value < -penalty)
+            return(value + penalty);
+        else 
+            return(0);
+    }
+    
+    void next_beta(Vector &res)
+    {
+        
+        int j;
+        for (j = 0; j < nvars; ++j)
+        {
+            beta(j) = 0;
+            double threshval = datX.col(j).dot(datY - datX * beta) / Xsq(j);
+            beta(j) = soft_threshold(threshval, lambda / Xsq(j));
+        }
+        
+        
+    }
+    
+    
+    // Calculate ||v1 - v2||^2 when v1 and v2 are sparse
+    static double diff_squared_norm(const SparseVector &v1, const SparseVector &v2)
+    {
+        const int n1 = v1.nonZeros(), n2 = v2.nonZeros();
+        const double *v1_val = v1.valuePtr(), *v2_val = v2.valuePtr();
+        const int *v1_ind = v1.innerIndexPtr(), *v2_ind = v2.innerIndexPtr();
+        
+        double r = 0.0;
+        int i1 = 0, i2 = 0;
+        while(i1 < n1 && i2 < n2)
+        {
+            if(v1_ind[i1] == v2_ind[i2])
+            {
+                double val = v1_val[i1] - v2_val[i2];
+                r += val * val;
+                i1++;
+                i2++;
+            } else if(v1_ind[i1] < v2_ind[i2]) {
+                r += v1_val[i1] * v1_val[i1];
+                i1++;
+            } else {
+                r += v2_val[i2] * v2_val[i2];
+                i2++;
+            }
+        }
+        while(i1 < n1)
+        {
+            r += v1_val[i1] * v1_val[i1];
+            i1++;
+        }
+        while(i2 < n2)
+        {
+            r += v2_val[i2] * v2_val[i2];
+            i2++;
+        }
+        
+        return r;
+    }
+    
+    
+public:
+    CoordLasso(ConstGenericMatrix &datX_, 
+               ConstGenericVector &datY_,
+               double tol_ = 1e-6) :
+    CoordBase(datX_.rows(), datX_.cols(),
+              tol_),
+              datX(datX_.data(), datX_.rows(), datX_.cols()),
+              datY(datY_.data(), datY_.size()),
+              XY(datX.transpose() * datY),
+              Xsq(datX.array().square().colwise().sum()),
+              lambda0(XY.cwiseAbs().maxCoeff())
+    {}
+    
+    double get_lambda_zero() const { return lambda0; }
+    
+    // init() is a cold start for the first lambda
+    void init(double lambda_)
+    {
+        beta.setZero();
+        
+        lambda = lambda_;
+    }
+    // when computing for the next lambda, we can use the
+    // current main_x, aux_z, dual_y and rho as initial values
+    void init_warm(double lambda_)
+    {
+        lambda = lambda_;
+    }
+};
+
+
+
+#endif // COORDLASSOTALL_H

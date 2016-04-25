05f2d37a98761e640a28c3af3e7f3ae5d43a3a9a
jaredhuling <huling@wisc.edu>
2016-04-24 21:55:16 -0500

    der updates

--- R/coord_mcp.R
+++ R/coord_mcp.R
@@ -385,3 +385,169 @@ predict.cv.cd.mcp = function(object, newx, which = c("parms.min", "parms.1se"), 
 }
 
 
+
+
+
+#' Fitting An MCP Model Using the Coordinate Descent Algorithm
+#' 
+#' @description Estimation of a linear model with the lasso penalty. The function
+#' \eqn{\beta} minimizes
+#' \deqn{\frac{1}{2n}\Vert y-X\beta\Vert_2^2+\lambda\Vert\beta\Vert_1}{
+#' 1/(2n) * ||y - X * \beta||_2^2 + \lambda * ||\beta||_1}
+#' 
+#' where \eqn{n} is the sample size and \eqn{\lambda} is a tuning
+#' parameter that controls the sparsity of \eqn{\beta}.
+#' 
+#' @param x The design matrix
+#' @param y The response vector
+#' @param intercept Whether to fit an intercept in the model. Default is \code{FALSE}. 
+#' @param standardize Whether to standardize the design matrix before
+#'                    fitting the model. Default is \code{FALSE}. Fitted coefficients
+#'                    are always returned on the original scale.
+#' @param lambda A user provided sequence of \eqn{\lambda}. If set to
+#'                      \code{NULL}, the program will calculate its own sequence
+#'                      according to \code{nlambda} and \code{lambda_min_ratio},
+#'                      which starts from \eqn{\lambda_0} (with this
+#'                      \eqn{\lambda} all coefficients will be zero) and ends at
+#'                      \code{lambda0 * lambda_min_ratio}, containing
+#'                      \code{nlambda} values equally spaced in the log scale.
+#'                      It is recommended to set this parameter to be \code{NULL}
+#'                      (the default).
+#' @param gamma Sparsity parameter vector
+#' @param penalty.factor a vector with length equal to the number of columns in x to be multiplied by lambda. by default
+#'                      it is a vector of 1s
+#' @param nlambda Number of values in the \eqn{\lambda} sequence. Only used
+#'                       when the program calculates its own \eqn{\lambda}
+#'                       (by setting \code{lambda = NULL}).
+#' @param lambda_min_ratio Smallest value in the \eqn{\lambda} sequence
+#'                                as a fraction of \eqn{\lambda_0}. See
+#'                                the explanation of the \code{lambda}
+#'                                argument. This parameter is only used when
+#'                                the program calculates its own \eqn{\lambda}
+#'                                (by setting \code{lambda = NULL}). The default
+#'                                value is the same as \pkg{glmnet}: 0.0001 if
+#'                                \code{nrow(x) >= ncol(x)} and 0.01 otherwise.
+#' @param maxit Maximum number of admm iterations.
+#' @param tol convergence tolerance parameter.
+#' @param rel.tol Relative tolerance parameter.
+#' 
+#' @examples set.seed(123)
+#' n = 1000
+#' p = 50
+#' b = c(runif(10), rep(0, p - 10))
+#' x = matrix(rnorm(n * p, sd = 3), n, p)
+#' y = drop(x %*% b) + rnorm(n)
+#' 
+#' ## fit lasso model with 100 tuning parameter values
+#' res <- cd.mcp(x, y, gamma = 4)
+#' 
+#' 
+#' @export
+cd.mcp.der <- function(x, 
+                   y, 
+                   lambda           = numeric(0), 
+                   gamma            = 4,
+                   penalty.factor,
+                   nlambda          = 100L,
+                   lambda.min.ratio = NULL,
+                   family           = c("gaussian", "binomial"),
+                   intercept        = FALSE,
+                   standardize      = FALSE,
+                   maxit            = 5000L,
+                   tol              = 1e-7
+)
+{
+    n <- nrow(x)
+    p <- ncol(x)
+    
+    x = as.matrix(x)
+    y = as.numeric(y)
+    intercept = as.logical(intercept)
+    standardize = as.logical(standardize)
+    family <- match.arg(family)
+    
+    if (n != length(y)) {
+        stop("number of rows in x not equal to length of y")
+    }
+    
+    if (missing(penalty.factor)) {
+        penalty.factor <- numeric(0)
+    } else {
+        if (length(penalty.factor) != p) {
+            stop("penalty.factor must be same length as number of columns in x")
+        }
+    }
+    
+    lambda_val = sort(as.numeric(lambda), decreasing = TRUE)
+    
+    if(any(lambda_val <= 0)) 
+    {
+        stop("lambda must be positive")
+    }
+    
+    if(nlambda[1] <= 0) 
+    {
+        stop("nlambda must be a positive integer")
+    }
+    
+    if(is.null(lambda.min.ratio))
+    {
+        lmr_val <- ifelse(nrow(x) < ncol(x), 0.01, 0.0001)
+    } else 
+    {
+        lmr_val <- as.numeric(lambda.min.ratio)
+    }
+    
+    if(lmr_val >= 1 | lmr_val <= 0) 
+    {
+        stop("lambda.min.ratio must be within (0, 1)")
+    }
+    
+    lambda           <- lambda_val
+    nlambda          <- as.integer(nlambda[1])
+    lambda.min.ratio <- lmr_val
+    
+    
+    if(maxit <= 0)
+    {
+        stop("maxit should be positive")
+    }
+    if(tol < 0)
+    {
+        stop("tol should be nonnegative")
+    }
+    
+    maxit   <- as.integer(maxit)
+    tol <- as.numeric(tol)
+    
+    if (family == "gaussian")
+    {
+        res <- .Call("coord_mcp_der", 
+                     x, y, 
+                     lambda,
+                     gamma,
+                     penalty.factor,
+                     nlambda, 
+                     lambda.min.ratio,
+                     standardize, intercept,
+                     list(maxit = maxit,
+                          tol   = tol),
+                     PACKAGE = "penreg")
+        lambda <- res$lambda
+        gamma  <- res$gamma
+        names(res$coefficients) <- paste0("g", 1:length(gamma))
+        parms <- array(NA, dim = c(2, length(gamma), length(lambda)))
+        parms[1,,] <- matrix(rep(gamma, length(lambda)), nrow = 2, byrow = FALSE)
+        parms[2,,] <- matrix(rep(lambda, length(gamma)), nrow = 2, byrow = TRUE)
+        dimnames(parms) <- list(c("gamma", "lambda"),
+                                paste0("g", 1:length(gamma)),
+                                paste0("l", 1:length(lambda)))
+        res$parms  <- parms
+    } else if (family == "binomial")
+    {
+        stop("Binomial not implemented yet")
+    }
+    class(res) <- "cd.mcp"
+    res
+}
+
--- src/CoordMCPder.h
+++ src/CoordMCPder.h
@@ -166,45 +166,6 @@ protected:
     }
     
     
-    // Calculate ||v1 - v2||^2 when v1 and v2 are sparse
-    static double diff_squared_norm(const SparseVector &v1, const SparseVector &v2)
-    {
-        const int n1 = v1.nonZeros(), n2 = v2.nonZeros();
-        const double *v1_val = v1.valuePtr(), *v2_val = v2.valuePtr();
-        const int *v1_ind = v1.innerIndexPtr(), *v2_ind = v2.innerIndexPtr();
-        
-        double r = 0.0;
-        int i1 = 0, i2 = 0;
-        while(i1 < n1 && i2 < n2)
-        {
-            if(v1_ind[i1] == v2_ind[i2])
-            {
-                double val = v1_val[i1] - v2_val[i2];
-                r += val * val;
-                i1++;
-                i2++;
-            } else if(v1_ind[i1] < v2_ind[i2]) {
-                r += v1_val[i1] * v1_val[i1];
-                i1++;
-            } else {
-                r += v2_val[i2] * v2_val[i2];
-                i2++;
-            }
-        }
-        while(i1 < n1)
-        {
-            r += v1_val[i1] * v1_val[i1];
-            i1++;
-        }
-        while(i2 < n2)
-        {
-            r += v2_val[i2] * v2_val[i2];
-            i2++;
-        }
-        
-        return r;
-    }
-    
     
 public:
     CoordMCPder(ConstGenericMatrix &datX_, 
